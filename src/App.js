09bf9f8fca812d265fe28e69e4ffcdf9c6ac1bcb
Vadim <NSPMSouL16@gmail.com>
2022-05-21 02:13:05 +0300

    pizza_5_full

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 import { Header } from './components';
 import { Home, Cart } from './components/pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then((response) => {
+      setPizzas(response.data.pizzas);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} exact />
+          <Route path="/" element={<Home pizzas={pizzas} />} exact />
           <Route path="/cart" element={<Cart />} exact />
         </Routes>
       </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import Categories from './Categories';
 import Button from './Button';
 import Header from './Header';
 import Sort from './Sort';
+import PizzaBlock from './pizzaBlock';
 
 // export {default as Button} from './Button'
 
-export { Categories, Button, Header, Sort };
+export { Categories, Button, Header, Sort, PizzaBlock };
